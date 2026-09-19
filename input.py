dd88157dd41ec0c3e4804a87f7f2ec836564b48d
Konstantin <bladflag8@gmail.com>
2026-09-19 12:09:23 +0300

    input

---
+++ input.py
@@ -0,0 +1,29 @@
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+#name = input('What is your name?: ')
+#age = int(input('How old are you?: '))
+
+# age = int(age)
+# age = age + 1
+
+#print(f'Hello {name}')
+#print("Happy birthday!")
+#print(f'You are {age} years old')
+
+# Exercise 1 Rectangle Area Calc
+#lenght = float(input('Enter the lenght: '))
+#width = float(input('Enter the width: '))
+#area = lenght * width
+
+#print(f'The area is: {area}cm^2')
+
+# Exercise 2 Shopping Cart Program
+
+item = input('What item would you like to buy?: ')
+price = float(input('What is the price?: '))
+quantity = int(input('How many would you like?: '))
+total = price * quantity
+
+print(f'You have bought {quantity} x {item}/s')
+print(f'Your total is: ${total}')
